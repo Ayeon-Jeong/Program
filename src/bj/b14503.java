c66d3b782400393809298f3a79be8ef6fc54f2eb
jdh96 <jdh961224@gmail.com>
2023-03-23 20:27:31 +0900

    코딩 연습

--- src/bj/b14503.java
+++ src/bj/b14503.java
@@ -1,69 +1,128 @@
 package bj;
 
-import HashMap.Sol;
-import HashMap.Solution;
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class b14503 {
 
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
-    static int N, M;
-    static boolean[][] visit;
-    static int[][] map;
-    static int answer = 0;
+    // 북 동 남 서
+    // 탐색할 위치: r + dx[d], c + dy[d]
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-    public static void Solution(int r, int c, int d){
+    static class Coor{
+        int r;
+        int c;
+        int d;
 
-        visit[r][c] = true;
-        for(int i = 0; i < 4; i++){
-            int dd = d-1;
-            if(dd < 0) dd += 4;
-
-            int nr = r + dy[i];
-            int nc = c + dx[i];
-
-            if(nr >= 0 && nc >= 00 && nr < N && nc < M){
-                if(!visit[nr][nc] && map[nr][nc] == 0){
-                    answer++;
-                    Solution(nr, nc, dd);
-                    return;
-                }
-            }
+        public Coor(int r, int c, int d) {
+            this.r = r;
+            this.c = c;
+            this.d = d;
         }
 
-        int dd = (d + 2) % 4; // 반대방향
-        int nr = r + dy[dd];
-        int nc = c + dx[dd];
-
-        if(nr >= 0 && nc >= 00 && nr < N && nc < M && map[nr][nc] == 0){
-            Solution(nr, nc, dd);
+        public int getR() {
+            return r;
         }
 
+        public void setR(int r) {
+            this.r = r;
+        }
 
+        public int getC() {
+            return c;
+        }
+
+        public void setC(int c) {
+            this.c = c;
+        }
+
+        public int getD() {
+            return d;
+        }
+
+        public void setD(int d) {
+            this.d = d;
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        solution(0);
+    }
 
-        N = in.nextInt(); M = in.nextInt();
+    private static void solution(int answer) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        visit = new boolean[N][M];
-        map = new int[N][M];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int r, c, d;
-        r = in.nextInt(); c = in.nextInt(); d = in.nextInt();
+        st = new StringTokenizer(bf.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                map[i][j] = in.nextInt();
-                visit[i][j] = false;
+        int[][] map = new int[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(bf.readLine());
+            for(int j = 0; j < m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        new b14503().Solution(r, c, d);
+        Coor robot = new Coor(r, c, d);
+        boolean flag = true;
+        while(flag){
+            int nowr = robot.getR();
+            int nowc = robot.getC();
+            int nowd = robot.getD();
+            // 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
+            if(map[nowr][nowc] == 0){
+                map[nowr][nowc] = -1;
+                answer++;
+            }
 
-        System.out.println(answer + 1);
+            boolean exist = false;
+            // 현재 칸의 주변 4칸 중
+            for(int i = 0; i < 4; i++){
+                // 주변 네칸 탐색
+                int nx = nowr + dx[i];
+                int ny = nowc + dy[i];
+
+                // 범위를 벗어나면 pass
+                if(nx >= n || nx < 0 || ny >= m || ny < 0)
+                    continue;
+                // 청소되지 않은 빈칸이 있으면
+                else if(map[nx][ny] == 0){
+                    // 반시계 방향으로 90도 회전
+                    exist = true;
+                    robot.setD((nowd + 3) % 4);
+                    nowd = robot.getD();
+                    // 회전 후 앞 칸이 청소되지 않은 빈 칸이면 전진한다
+                    if(map[nowr + dx[nowd]][nowc + dy[nowd]] == 0){
+                        robot.setR(nowr + dx[nowd]);
+                        robot.setC(nowc + dy[nowd]);
+                    }
+                    break;
+                }
+
+                if(i == 3 && !exist){
+                    // 청소되지 않은 빈칸이 없으면
+                    // 바라보는 방향의 반대방향 = (d + 2) % 4
+                    if(map[nowr + dx[(nowd + 2) % 4]][nowc + dy[(nowd + 2) % 4]] == 1){
+                        flag = false;
+                    } else{
+                        robot.setR(nowr + dx[(nowd + 2) % 4]);
+                        robot.setC(nowc + dy[(nowd + 2) % 4]);
+                    }
+                }
+            }
+
+
+        }
+        System.out.println(answer);
+
     }
 }
